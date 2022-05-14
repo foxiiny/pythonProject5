3163e04bf64c74ea31a1049811f007151921f94b
foxiiny <105548988+foxiiny@users.noreply.github.com>
2022-05-15 00:44:11 +1000

    Add files via upload

---
+++ 111.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+@st.cache
+def get_data():
+    return pd.read_csv('kpop_idols.csv')
+
+
+df = get_data()
+
+
+country = st.selectbox('Country', df['Country'].value_counts().iloc[:5].index)
+
+
+df_selection1 = df[lambda x: x['Country'] == country]
+df_selection1
+
+
+group = st.selectbox('Group', df['Group'].value_counts().index)
+df_selection2 = df[lambda x: x['Group'] == group]
+df_selection2
+
+
+
+
